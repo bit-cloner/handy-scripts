bd276ea695ee8abbbebbc64a317a91ed0f2ce0f3
bit-cloner <62158497+bit-cloner@users.noreply.github.com>
2020-11-04 13:40:02 +0000

    Update install-exa-rust-ls.sh

--- install-exa-rust-ls.sh
+++ install-exa-rust-ls.sh
@@ -2,7 +2,7 @@
 wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
 unzip  exa-linux-x86_64-0.9.0.zip
 mv exa-linux-x86_64 exa
-sudo mv exa-linux-x86_64 /usr/local/bin/
+sudo mv exa /usr/local/bin/
 exa
 alias rls="exa"
 
